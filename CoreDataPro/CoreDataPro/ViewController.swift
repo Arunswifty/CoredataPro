742fc1e9c4a1149fa3cefb973ea33a09ad2e6bfa
Arun Krishnan M <31237717+Arunswifty@users.noreply.github.com>
2019-03-12 16:19:50 +0530

    Add files via upload

---
+++ CoreDataPro/CoreDataPro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataPro
+//
+//  Created by ArunK on 12/03/19.
+//  Copyright © 2019 ArunK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
